cc59cde9b67fd8abbd4b835c0076bf9e59bcf072
bababahadir <theworld1716@gmail.com>
2025-08-29 01:27:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,2 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  redirect('/tr');
-}
+export default function RootPage() { 
+Redirect to default locale (Turkish) redirect('/tr') }
