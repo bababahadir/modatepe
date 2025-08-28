4af1fbe8375d166d12a4c1084b0a5d9dec6ab66b
bababahadir <theworld1716@gmail.com>
2025-08-29 01:26:36 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
