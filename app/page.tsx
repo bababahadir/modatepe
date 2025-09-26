539c1eeaa7bea4e55a629e879779639582d52836
bababahadir <theworld1716@gmail.com>
2025-09-26 18:08:44 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,4 @@
-export default function RootPage() { 
-Redirect to default locale (Turkish) redirect('/tr') }
+export default function RootPage() {
+  // Redirect to default locale (Turkish)
+  redirect('/tr');
+}
